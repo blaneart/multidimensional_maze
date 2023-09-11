1d7e40cd816d5fcd806dc3aacc3a17bdda80b3c3
blaneart <blanar.a.v@gmail.com>
2023-09-11 14:47:12 +0200

    name change

--- example.py
+++ example.py
@@ -8,16 +8,11 @@ for w in np.arange(2, 3.0, 0.25):
     with cProfile.Profile() as pr:
         np.random.seed(23)
         my_path_finder = PathFinder3D(diagonal=False)    
-        # maze_3d = np.random.randint(low=0, high=2, )
-
         maze_3d = np.random.choice([0, 1], size=(3, 5, 9), p=[0.7, 0.3])
         stop = [2, 4, 8]
         start = [0, 0, 0]
         maze_3d[stop[0], stop[1], stop[2]] = 0
-        print(maze_3d)
-        print(maze_3d[stop[0], stop[1], stop[2]], maze_3d[start[0], start[1], start[2]])
-        path = my_path_finder.find_path(maze_3d, start=start, stop=stop, w=w, verbose=True)
-        print(path)
+        path = my_path_finder.find_path(maze_3d, start=start, stop=stop, w=w)
         pr.disable()
         s = io.StringIO()
         sortby = SortKey.TIME
